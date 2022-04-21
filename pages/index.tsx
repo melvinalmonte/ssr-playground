cda39c113858b4a381b194c5ada186b2e2370bc4
Melvin A <melvinalmonte@gmail.com>
2022-04-21 00:21:58 -0400

    SSR commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,15 +6,17 @@ import styles from "../styles/Home.module.css";
 
 const s3URL = "http://melvs-pokemon.s3-website-us-east-1.amazonaws.com";
 
-const Home: NextPage = () => {
-  const [pokemon, setPokemon] = React.useState([]);
-  React.useEffect(() => {
-    getPokemon();
-  }, []);
-  async function getPokemon() {
-    const resp = await fetch(`${s3URL}/index.json`);
-    setPokemon(await resp.json());
-  }
+// It makes any requests to any services and returns an object that has props which then gets send to our react component
+export async function getServerSideProps() {
+  const resp = await fetch(`${s3URL}/index.json`);
+  return {
+    props: {
+      pokemon: await resp.json(),
+    },
+  };
+}
+
+const Home: NextPage = ({ pokemon }) => {
   return (
     <div className={styles.container}>
       <Head>
--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,28 +1,21 @@
 import React from "react";
-import { useRouter } from "next/router";
 import Head from "next/head";
 import Link from "next/link";
 import styles from "../../styles/Details.module.css";
 
 const s3URL = "http://melvs-pokemon.s3-website-us-east-1.amazonaws.com";
-export default function Details() {
-  const {
-    query: { id },
-  } = useRouter();
-  const [pokemon, setPokemon] = React.useState(null);
-  React.useEffect(() => {
-    async function getPokemon() {
-      const resp = await fetch(`${s3URL}/pokemon/${id}.json`);
-      setPokemon(await resp.json());
-    }
-    if (id) {
-      getPokemon();
-    }
-  }, [id]);
 
-  if (!pokemon) {
-    return null;
-  }
+// this function gets called first, once fulfilled it calls and renders our react component
+export async function getServerSideProps({ params }) {
+  const resp = await fetch(`${s3URL}/pokemon/${params.id}.json`);
+  return {
+    props: {
+      pokemon: await resp.json(),
+    },
+  };
+}
+
+export default function Details({ pokemon }) {
   return (
     <div>
       <Head>
